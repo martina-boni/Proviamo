c6dc3e2677a2653c760ccd4ef543de884e41bff4
martina-boni <martina.boni@student.h-farm.com>
2021-11-24 12:56:25 +0100

    modified importazione dati

--- importazionedati.py
+++ importazionedati.py
@@ -1,1 +1,13 @@
+pip install python-calendarific
 
+import calendarific
+
+calapi = calendarific.v2('6981d02afb34fb0d7f67b5ad93491ab8283a7773')
+
+parameters = {
+	# Required
+	'country': 'US',
+	'year':    2019,
+}
+
+holidays = calapi.holidays(parameters)
