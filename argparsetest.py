7a21fc871d1d6c300b8d6b6a51e13c5cef760996
martina-boni <martina.boni@student.h-farm.com>
2021-11-30 17:16:05 +0100

    modified file

--- argparsetest.py
+++ argparsetest.py
@@ -98,13 +98,3 @@ def return_event(date):
 # return_event('2023-11-01')
 
 
-
-import argparse
-
-valid_dates = dtvnt.keys()
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='A program to pass dates to the return_event function')
-    parser.add_argument("date", help="The string of the date in the form yyyy-mm-dd", choices=valid_dates)
-    args = parser.parse_args()
-    return return_event(args.date)
